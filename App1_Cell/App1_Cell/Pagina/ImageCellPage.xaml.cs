23c787b5d5a43fac33b7b4e9f464c10cc4bfa813
Joao Paulo <joaopaulo.fr.1994@gmail.com>
2021-07-10 19:51:22 -0300

    Adicionar arquivos de projeto.

---
+++ App1_Cell/App1_Cell/Pagina/ImageCellPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using App1_Cell.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Pagina
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class ImageCellPage : ContentPage
+  {
+    public ImageCellPage()
+    {
+      InitializeComponent();
+
+      List<Funcionario> Lista = new List<Funcionario>();
+      Lista.Add(new Funcionario() { Foto = "f1.jpg", Nome = "José", Cargo = "Presidente da EMpresa" });
+      Lista.Add(new Funcionario() { Nome = "Maria", Cargo = "Gerente de Vendas" });
+      Lista.Add(new Funcionario() { Nome = "Elaine", Cargo = "Gerente de Marqueting" });
+      Lista.Add(new Funcionario() { Foto = "f2.jpg", Nome = "Felipe", Cargo = "Entregador" });
+      Lista.Add(new Funcionario() { Foto = "f3.jpg", Nome = "João", Cargo = "Vendedor" });
+
+      ListaFuncionario.ItemsSource = Lista;
+    }
+  }
+}
---
+++ App1_Cell/App1_Cell/Pagina/ListViewPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using App1_Cell.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1_Cell.Pagina
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class ListViewPage : ContentPage
+  {
+    public ListViewPage()
+    {
+      InitializeComponent();
+
+      List<Funcionario> Lista = new List<Funcionario>();
+      Lista.Add(new Funcionario() { Nome = "José", Cargo = "Presidente da EMpresa" });
+      Lista.Add(new Funcionario() { Nome = "Maria", Cargo = "Gerente de Vendas" });
+      Lista.Add(new Funcionario() { Nome = "Elaine", Cargo = "Gerente de Marqueting" });
+      Lista.Add(new Funcionario() { Nome = "Felipe", Cargo = "Entregador" });
+      Lista.Add(new Funcionario() { Nome = "João", Cargo = "Vendedor" });
+
+      ListaFuncionario.ItemsSource = Lista;
+    }
+    private void itemSelecionadoAction(object sender, SelectedItemChangedEventArgs args)
+    {
+      Funcionario func = (Funcionario)args.SelectedItem;
+
+      Navigation.PushAsync(new Detail.DetailPage(func));
+    }
+
+    private void FeriasAction(object sender, EventArgs args)
+    {
+      MenuItem botao = (MenuItem)sender;
+      Funcionario func = (Funcionario)botao.CommandParameter;
+
+      DisplayAlert("Título: " + func.Nome, "Mensagem: " + func.Cargo, "OK");
+    }
+    private void AbonoAction(object sender, EventArgs args)
+    {
+
+    }
+  }
+}
